d5cdfdd0caba7303585300def13932d923a9ce02
jesussiero <jesussiero@gmail.com>
2024-02-11 22:41:19 +0100

    Add files via upload

---
+++ LION PRO/v1.18 MotorNormal/Marlin/Default_Version.h
@@ -0,0 +1,14 @@
+/*
+ * This file is a placeholder for a file which could be distributed in an archive
+ * It takes the place of an automatically created "_Version.h" which is generated during the build process
+ */
+
+// #error "You must specify the following parameter related to your distribution"
+
+#if true
+  #define FIRMWARE_VERSION "v1.18"   //Leon 3D firmware version
+  #define BUILD_VERSION "1.0.3 dev"  //Marlin version
+  #define STRING_DISTRIBUTION_DATE "2016-08-18 13:25"
+  // It might also be appropriate to define a location where additional information can be found
+  #define SOURCE_CODE_URL  "http://www.leon-3d.es/"
+#endif
